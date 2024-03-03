23faa289ff9c827910b8882f4e4fe4e65f51c219
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-03-02 20:46:18 -0600

    Slight cleanup

--- modules/projects/projects.go
+++ modules/projects/projects.go
@@ -2,6 +2,7 @@ package projects
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -46,38 +47,12 @@ type Release struct {
 	URL     string `json:"html_url"`
 }
 
-// Forge Mod Updates Format
-type ForgeModUpdates struct {
-	Homepage string            `json:"homepage"`
-	Promos   map[string]string `json:"promos"`
-	V1_7_10  map[string]string `json:"1.7.10"`
-	V1_8_9   map[string]string `json:"1.8.9"`
-	V1_9_4   map[string]string `json:"1.9.4"`
-	V1_10_2  map[string]string `json:"1.10.2"`
-	V1_11_2  map[string]string `json:"1.11.2"`
-	V1_12_2  map[string]string `json:"1.12.2"`
-	V1_13_2  map[string]string `json:"1.13.2"`
-	V1_14_4  map[string]string `json:"1.14.4"`
-	V1_15_2  map[string]string `json:"1.15.2"`
-	V1_16_5  map[string]string `json:"1.16.5"`
-	V1_17_1  map[string]string `json:"1.17.1"`
-	V1_18    map[string]string `json:"1.18"`
-	V1_18_1  map[string]string `json:"1.18.1"`
-	V1_18_2  map[string]string `json:"1.18.2"`
-	V1_19    map[string]string `json:"1.19"`
-	V1_19_1  map[string]string `json:"1.19.1"`
-	V1_19_2  map[string]string `json:"1.19.2"`
-	V1_19_3  map[string]string `json:"1.19.3"`
-	V1_19_4  map[string]string `json:"1.19.4"`
-	V1_20    map[string]string `json:"1.20"`
-	V1_20_1  map[string]string `json:"1.20.1"`
-	V1_20_2  map[string]string `json:"1.20.2"`
-	V1_20_3  map[string]string `json:"1.20.3"`
-	V1_20_4  map[string]string `json:"1.20.4"`
-}
-
 // Functions
 func GetReleases(group string, project string) ([]Release, error) {
+	if githubToken == "" {
+		return nil, errors.New("GITHUB_TOKEN is not set")
+	}
+
 	githubURL := "https://api.github.com/repos/" + group + "/" + project + "/releases"
 	req, err := http.NewRequest("GET", githubURL, nil)
 	if err != nil {
@@ -105,8 +80,8 @@ func GetReleases(group string, project string) ([]Release, error) {
 }
 
 // Convert to Forge Mod Updates Format
-func ConvertToForgeModUpdates(gitHubReleasesURL string, releases []Release) map[string]interface{} {
-	forgeModUpdates := make(map[string]interface{})
+func ConvertToFMLFormat(gitHubReleasesURL string, releases []Release) map[string]interface{} {
+	fmlFormat := make(map[string]interface{})
 
 	releaseMap := make(map[string]string)
 	for _, release := range releases {
@@ -120,13 +95,13 @@ func ConvertToForgeModUpdates(gitHubReleasesURL string, releases []Release) map[
 		promosMap[version+"-recommended"] = releases[0].URL
 	}
 
-	forgeModUpdates["homepage"] = gitHubReleasesURL
-	forgeModUpdates["promos"] = promosMap
+	fmlFormat["homepage"] = gitHubReleasesURL
+	fmlFormat["promos"] = promosMap
 	for _, version := range forgeModVersions {
-		forgeModUpdates[version] = releaseMap
+		fmlFormat[version] = releaseMap
 	}
 
-	return forgeModUpdates
+	return fmlFormat
 }
 
 // Handlers
@@ -144,7 +119,7 @@ func GetReleasesHandler(c echo.Context) error {
 
 	if format == "fml" {
 		gitHubReleasesURL := "https://github.com/" + group + "/" + project + "/releases"
-		forgeModUpdates := ConvertToForgeModUpdates(gitHubReleasesURL, releases)
+		forgeModUpdates := ConvertToFMLFormat(gitHubReleasesURL, releases)
 		return c.JSON(http.StatusOK, forgeModUpdates)
 	}
 	return c.JSON(http.StatusOK, releases)
