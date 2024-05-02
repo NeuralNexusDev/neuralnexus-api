0cf842e7a3b723e9767dc9f50b848713f2e2b05a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-05-01 23:28:37 -0600

    Generalized Bukkit client

--- modules/archive/bucket.go
+++ modules/archive/bucket.go
@@ -3,40 +3,26 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
+	"github.com/NeuralNexusDev/neuralnexus-api/modules/database"
 	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func main() {
+	minioClient := database.GetS3()
+
 	ctx := context.Background()
-	endpoint := os.Getenv("S3_API_URL")
-	accessKeyID := os.Getenv("S3_ACCESS_KEY")
-	secretAccessKey := os.Getenv("S3_SECRET_KEY")
-	useSSL := os.Getenv("S3_USE_SSL") == "true"
-
-	// Initialize minio client object.
-	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		Secure: useSSL,
-	})
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// Make a new bucket called testbucket.
 	bucketName := "minecraft-archive"
-	location := "us-east-1"
+	location := "ca-central-1"
 
-	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	err := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
 	if err != nil {
 		// Check to see if we already own this bucket (which happens if you run this twice)
 		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
 		if errBucketExists == nil && exists {
 			log.Printf("We already own %s\n", bucketName)
 		} else {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 	} else {
 		log.Printf("Successfully created %s\n", bucketName)
--- modules/database/snowflake.go
+++ modules/database/snowflake.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"log"
+	"os"
 	"strconv"
 
 	"github.com/kkrypt0nn/spaceflake"
@@ -12,8 +14,18 @@ var settings = initSnowflake()
 func initSnowflake() spaceflake.GeneratorSettings {
 	settings := spaceflake.NewGeneratorSettings()
 	settings.BaseEpoch = 1706639400000 // January 30, 2024 12:30:00 PM Central/Regina
-	settings.NodeID = 1
-	settings.WorkerID = 1
+	nodeID, err := strconv.ParseUint(os.Getenv("SNOWFLAKE_NODE_ID"), 10, 64)
+	if err != nil {
+		nodeID = 1
+		log.Println("Failed to parse SNOWFLAKE_NODE_ID, defaulting to 1")
+	}
+	settings.NodeID = nodeID
+	workID, _ := strconv.ParseUint(os.Getenv("SNOWFLAKE_WORKER_ID"), 10, 64)
+	if err != nil {
+		workID = 1
+		log.Println("Failed to parse SNOWFLAKE_WORKER_ID, defaulting to 1")
+	}
+	settings.WorkerID = workID
 	settings.Sequence = 0
 	return settings
 }
