03e37721f3c4ebf2c67d5d533da0d826b92cfb09
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-09-14 12:13:00 -0600

    Improved types and added helper methods

--- modules/minecraft/types.go
+++ modules/minecraft/types.go
@@ -1,6 +1,12 @@
 package minecraft
 
-import "errors"
+import (
+	"encoding/base64"
+	"errors"
+	"log"
+
+	"github.com/goccy/go-json"
+)
 
 var ErrPlayerNotFound = errors.New("player not found")
 
@@ -16,20 +22,55 @@ type Player struct {
 	LastSeen       int64      `json:"-"                    db:"last_seen"`
 }
 
+// ParseProperties parse the player's properties
+func (p *Player) ParseProperties() (*TexturesValue, error) {
+	for _, prop := range p.Properties {
+		if prop.Name != TEXTURE {
+			log.Println("Unknown property:\n\t" + prop.String())
+			continue
+		}
+		decoded, err := base64.StdEncoding.DecodeString(prop.Value)
+		if err != nil {
+			return nil, err
+		}
+		var textures TexturesValue
+		if err := json.Unmarshal(decoded, &textures); err != nil {
+			return nil, err
+		}
+		return &textures, nil
+	}
+	return nil, nil
+}
+
 // Property - A player property as returned by Mojang
 type Property struct {
-	Name      string `json:"name"`
-	Value     string `json:"value"`
-	Signature string `json:"signature,omitempty"`
+	Name      PropertyName `json:"name"`
+	Value     string       `json:"value"`
+	Signature string       `json:"signature,omitempty"`
 }
+
+func (p *Property) String() string {
+	str := "{ Name: " + string(p.Name) + ", Value: " + p.Value
+	if p.Signature != "" {
+		str += ", " + p.Signature
+	}
+	str += " }"
+	return str
+}
+
+// PropertyName type alias
+type PropertyName string
+
+// TEXTURE the only known in-use value returned from the Mojang API
+const TEXTURE PropertyName = "texture"
 
 // TexturesValue - Decoded textures property
 type TexturesValue struct {
-	Timestamp         int64     `json:"timestamp"`
-	ProfileID         string    `json:"profileId"`
-	ProfileName       string    `json:"profileName"`
-	SignatureRequired bool      `json:"signatureRequired,omitempty"`
-	Textures          *Textures `json:"textures"`
+	Timestamp         int64    `json:"timestamp"`
+	ProfileID         string   `json:"profileId"`
+	ProfileName       string   `json:"profileName"`
+	SignatureRequired bool     `json:"signatureRequired,omitempty"`
+	Textures          Textures `json:"textures"`
 }
 
 // Textures - The player's textures
@@ -46,5 +87,11 @@ type Texture struct {
 
 // Metadata - Skin metadata (only present for Alex/slim model)
 type Metadata struct {
-	Model string `json:"model"`
+	Model Model `json:"model"`
 }
+
+// Model type alias
+type Model string
+
+// SLIM The only known value for Metadata.Model
+const SLIM Model = "slim"
